7aa1f1457c5fe2d4b71da1dd4c17a8def2c87a53
Puckanchik <66815833+Puckanchik@users.noreply.github.com>
2021-02-22 13:35:07 +0600

    Add files via upload

---
+++ WeatherApp-main/as4/CollectionViewCell/WeatherCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherCollectionViewCell.swift
+//  lecture9
+//
+//  Created by Adil on 15.02.2021.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "WeatherCollectionViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "WeatherCollectionViewCell",
+                     bundle: nil)
+    }
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+
+    func configure(with model: HourlyWeatherEntry) {
+        self.tempLabel.text = "\(model.temperature)"
+        self.iconImageView.contentMode = .scaleAspectFit
+        self.iconImageView.image = UIImage(named: "clear")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
